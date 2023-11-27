9b756a6c462a06a30c1bdbf4cbeafb4468a1f842
MDubach <michadubach@gmail.com>
2023-11-27 11:10:16 +0100

    first try

---
+++ javascript.js
@@ -0,0 +1,12 @@
+
+
+function generateDivinDiv() {
+
+    var newDiv = document.createElement("div");
+
+    var div = document.getElementById("A1");
+    newDiv.className = "playerPoint01"
+    div.appendChild(newDiv);
+}
+
+generateDivinDiv();
