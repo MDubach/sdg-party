e27d28efe411f2916cbdcbee3195d49c16049584
MDubach <michadubach@gmail.com>
2023-11-27 13:38:06 +0100

    first script js

--- javascript.js
+++ javascript.js
@@ -1,12 +1,55 @@
 
 
-function generateDivinDiv() {
+var playerNumber = 2;
+
+var playerColors = ["red", "blue", "green", "pink"];
+
+
+function startGame(numberOfPlayers) {
+
+    // set playerNumber
+    playerNumber = numberOfPlayers;
+
+    setAllPlayerPoints();
+
+    showInfoField();
+
+}
+
+function setAllPlayerPoints() {
+
+    for (var i = 1; i <= playerNumber; i++) {
+        generatePlayerPointsAsDiv(playerColors[i -1], i);
+    }
+
+}
+
+function generatePlayerPointsAsDiv(playerColor, number) {
+    console.log(playerColor);
 
     var newDiv = document.createElement("div");
 
-    var div = document.getElementById("A1");
-    newDiv.className = "playerPoint01"
+    var div = document.getElementById("F1");
+    newDiv.id = "player" + number;
+    newDiv.className = "playerPoint" + number;
     div.appendChild(newDiv);
 }
 
-generateDivinDiv();
+function showInfoField() {
+    alert("los gehts");
+}
+
+
+
+
+function getRandomDiceNumber() {
+    return Math.floor(Math.random() * 6) + 1;
+}
+
+var randomValue = getRandomDiceNumber();
+console.log(randomValue);
+
+
+
+
+
