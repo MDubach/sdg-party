3ba6b458f600f189ac4c49686f4e581eceedc1be
MDubach <michadubach@gmail.com>
2023-11-28 12:37:44 +0100

    javascript new stuff

--- javascript.js
+++ javascript.js
@@ -2,8 +2,10 @@
 var pathBig = ["F1", "F2", "F3", "F4", "F5", "E5", "E6", "D6", "C6", "B6", "B5", "A5", "A4", "A3", "A2", "A1", "B1", "C1", "D1", "E1", "F1"];
 var pathMiddle = ["F1", "F2", "F3", "F4", "F5", "E5", "E6", "D6", "C6", "B6", "B5", "A5", "A4", "A3", "B3", "C3", "C2", "C1", "D1", "E1", "F1"];
 var pathShort = ["F1", "F2", "F3", "F4", "F5", "E5", "D4", "C4", "C3", "C2", "C1", "D1", "E1", "F1"];
+var allPossibleFields = ["A1", "A2", "A3", "A4", "A5", "B1", "B3", "B5", "B6", "C1", "C2", "C3", "C4", "C6", "D1", "D4", "D6", "E1", "E5", "E6", "F1", "F2", "F3", "F4", "F5"];
 
 var playerNumber = 2;
+var fieldWithSDG = "";
 
 var players = [
     { color: "red", number: 1, position: "F1", positionInArray: 0, choosenPath: pathBig, sdgCards: 0 },
@@ -12,7 +14,15 @@ var players = [
     { color: "yellow", number: 4, position: "F1", positionInArray: 0, choosenPath: pathBig, sdgCards: 0 }
 ];
 
+var sdgArray = [
+    { url: "hello1"},
+    { url: "hello2" },
+    { url: "hello3" },
+    { url: "hello4" }
+];
+
 var playerTurnIndex = 0;
+var sdgAvailableIndex = 0;
 
 function startGame(numberOfPlayers) {
 
@@ -26,11 +36,14 @@ function startGame(numberOfPlayers) {
     // create all player points on map
     setAllPlayerPoints();
 
+    // set first sdg goal on map
+    setSdgGoalOnMap();
+
     // hide foreground-container
-    var buttonContainer = document.getElementById("button-container");
+    var buttonContainer = document.getElementById("player-container");
     buttonContainer.style.display = "none";
     var diceContainer = document.getElementById("dice-container");
-    diceContainer.style.display = "initial";
+    diceContainer.style.display = "flex";
 
 }
 
@@ -44,20 +57,41 @@ function setAllPlayerPoints() {
 
 function generatePlayerPointsAsDiv(number, fieldId) {
     var newDiv = document.createElement("div");
-
     var div = document.getElementById(fieldId);
     newDiv.id = "player" + number;
     newDiv.className = "playerPoint";
     div.appendChild(newDiv);
 }
 
-function getRandomDiceNumber() {
-    return Math.floor(Math.random() * 6) + 1;
+function setSdgGoalOnMap() {
+
+    var setOnThisFields = allPossibleFields;
+
+    for (var i = 0; i < playerNumber; i++) {
+        var player = players[i];
+        let valueToRemove = player.choosenPath[player.positionInArray];
+        let indexToRemove = setOnThisFields.indexOf(valueToRemove);
+
+        if (indexToRemove !== -1) {
+            setOnThisFields.splice(indexToRemove, 1);
+            console.log("Value removed:", valueToRemove);
+        } else {
+            console.log("Value not found in the array");
+        }
+
+        console.log(setOnThisFields);        
+    }
+
+    let randomIndex = Math.floor(Math.random() * setOnThisFields.length);
+    let randomField = setOnThisFields[randomIndex];
+
+    fieldWithSDG = randomField;
+
+    var fieldToPlaceSDG = document.getElementById(randomField);
+    fieldToPlaceSDG.style.backgroundColor = "black";
+
+    console.log(randomField);
 }
-
-var randomValue = getRandomDiceNumber();
-console.log(randomValue);
-
 
 async function rollDice() {
 
@@ -74,6 +108,11 @@ async function rollDice() {
     
 }
 
+function getRandomDiceNumber() {
+    return Math.floor(Math.random() * 6) + 1;
+}
+
+
 async function playerTurn(resultDice) {
     showPlayboard();
     console.log("player turn wait on click");
@@ -85,13 +124,22 @@ async function playerTurn(resultDice) {
 function showPlayboard() {
     var foregroundContainer = document.getElementById("foreground-container");
     foregroundContainer.style.display = "none";
+    var diceContainer = document.getElementById("dice-container");
+    diceContainer.style.display = "none";
+
 }
 
 function showDiceContainer() {
     var foregroundContainer = document.getElementById("foreground-container");
-    foregroundContainer.style.display = "initial";
+    foregroundContainer.style.display = "flex";
+    var buttonRollDice = document.getElementById("dice-number");
+    buttonRollDice.innerHTML = "";
+    var diceContainer = document.getElementById("dice-container");
+    diceContainer.style.display = "flex";
     var buttonRollDice = document.getElementById("btn-roll-dice");
-    buttonRollDice.style.display = "initial";
+    buttonRollDice.style.display = "flex";
+    var infoPlayerTurnText = document.getElementById("info-player-turn-text");
+    infoPlayerTurnText.innerHTML = `Spieler:in ${playerTurnIndex + 1} ist dran!`;
 }
 
 async function movePlayerPoint(resultDice) {
@@ -111,16 +159,31 @@ async function movePlayerPoint(resultDice) {
             // show foreground auswahl
             console.log("wait on click");
 
-            await waitOnSelectDirection();
+            var foregroundContainer = document.getElementById("foreground-container");
+            foregroundContainer.style.display = "flex";
+            var selectDirectionContainer = document.getElementById("select-direction-container");
+            selectDirectionContainer.style.display = "flex";
+
+            await waitOnSelectDirection(playerPosition, player);
+
+            foregroundContainer.style.display = "none";
+            selectDirectionContainer.style.display = "none";
 
             console.log("clicked");
+            console.log(player);
         }
 
         player.positionInArray++;
+
+        if (player.choosenPath[player.positionInArray] == fieldWithSDG) {
+            alert("lutsch eier");
+        }
         
     }
 
     movePointByOneField(player);
+
+    
 
     playerTurnIndex++;
 
@@ -148,14 +211,34 @@ function movePointByOneField(player) {
     div.appendChild(newDiv);
 }
 
-
-function waitOnSelectDirection(elementId) {
+function waitOnSelectDirection(playerPosition, player) {
     return new Promise(resolve => {
-      document.getElementById(elementId).addEventListener('click', () => {
-        resolve();
-      });
-    });
-}
+      const leftIcon = document.getElementById("left-direction-icon");
+      const rightIcon = document.getElementById("right-direction-icon");
   
+      const selectedLeftSide = () => {
+        leftIcon.removeEventListener('click', selectedLeftSide);
+        rightIcon.removeEventListener('click', selectedRightSide);
+
+        if (playerPosition == "E5") {
+            player.choosenPath = pathShort;
+        } else {
+            player.choosenPath = pathMiddle;
+        }
+
+        resolve();
+      };
+
+      const selectedRightSide = () => {
+        leftIcon.removeEventListener('click', selectedLeftSide);
+        rightIcon.removeEventListener('click', selectedRightSide);
+
+        resolve();
+      }
+
+      leftIcon.addEventListener('click', selectedLeftSide);
+      rightIcon.addEventListener('click', selectedRightSide);
+    });
+  }
 
 
